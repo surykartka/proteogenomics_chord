a5bfba3e99a16895af153e95e774ba61ceb24d91
surykartka <surykartka@gmail.com>
2022-09-08 19:25:57 +0200

    chip diagram

--- chip/make_chord.py
+++ chip/make_chord.py
@@ -1,7 +1,7 @@
 import csv
 
 chip_file = 'data/Master_CHIP.csv'
-pvalue_threshold = 1e-4
+pvalue_threshold = 1e-8
 
 output_file = 'circos/chip_links.txt'
 output_labels_file = 'circos/chip_labels.txt'
